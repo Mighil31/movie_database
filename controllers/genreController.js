e0893caedc6a045792e4afd4a42ba14560bec495
mighil31 <mighildath@gmail.com>
2020-01-02 16:02:27 +0530

    Add genre detail page

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,4 +1,7 @@
 var Genre = require('../models/genre');
+var Movie = require('../models/movie');
+
+var async = require('async');
 
 // Display list of all Genre.
 exports.genre_list = function(req, res) {
@@ -11,7 +14,28 @@ exports.genre_list = function(req, res) {
 
 // Display detail page for a specific Genre.
 exports.genre_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre detail: ' + req.params.id);
+    
+    async.parallel({
+        genre: function(callback) {
+            Genre.findById(req.params.id)
+              .exec(callback);
+        },
+
+        genre_movies: function(callback) {
+            Movie.find({ 'genre': req.params.id })
+              .exec(callback);
+        },
+
+    }, function(err, results) {
+        if (err) { return next(err); }
+        if (results.genre==null) { // No results.
+            var err = new Error('Genre not found');
+            err.status = 404;
+            return next(err);
+        }
+        // Successful, so render
+        res.render('genre_detail', { title: 'Genre Detail', genre: results.genre, genre_movies: results.genre_movies } );
+    });
 };
 
 // Display Genre create form on GET.
